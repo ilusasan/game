cd56ebbf37f236d44c4df150a310bbee4510cb9f
ilusasan <63187377+ilusasan@users.noreply.github.com>
2020-04-08 13:59:29 +0300

    Add files via upload

--- Game.py
+++ Game.py
@@ -10,6 +10,76 @@ BEX=0, 255, 255
 BBLUE=0, 255, 0
 PLOTFORM=139, 69, 19
 
+class Player:
+	def __init__(self, platform, color=BEX):
+		self.color = color
+		self.x = 100
+		self.y = 450
+		self.speed = 4
+		self.w = 25
+		self.h = 25
+		self.jumpHeight = 200
+		self.state = "stop"
+		self.platform = platform
+		self.jump = False
+		self.tempjh = self.y + self.jumpHeight
+
+	def collision(self,object):
+		for o in object:
+			if self.y + self.h >= o.y:
+				col = (True,"down")
+			elif self.y <= o.y + o.h:
+				col = (True,"up")
+			elif self.x + self.w >= o.x:
+				col = ( True,"right")
+			elif self.x <= o.x + o.w:
+				col = (True,"left")
+			else:
+				col = (False,"")
+			if col[0] == True:
+				break
+		return col
+
+
+
+	def  move(self):
+		if (self.state == "left" 
+		and not(self.collision(self.platform)[1] == "left")):
+			self.x -= self.speed
+		elif (self.state == "right"
+			and not(self.collision(self.platform)[1] == "right")):
+			self.x += self.speed
+		if (self.collision(self.platform)[1] == "down"):
+			self.tempjh = self.y + self.jumpHeight
+			
+		else :
+			self.y += 3
+		if (self.jump == True):
+			if self.y <= self.tempjh:
+				self.y -= 6
+			else:
+				self.jump = False
+
+	def draw(self):
+		self.move()
+		pygame.draw.rect(s, self.color, (self.x, self.y, self.w, self.h))
+
+class Platform:
+	def __init__(self,x,y,w,h,color):
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+		self.color = color
+
+	def draw(self):
+		pygame.draw.rect(s, self.color, (self.x, self.y, self.w, self.h))
+
+
+
+gr = Platform(0,490,750,10,PLOTFORM)
+gr1 = Platform(400,250,200,100,(255, 255, 255))
+pl = Player(platform = [gr,gr1])
 while True:
 	for event in pygame.event.get():
 		if event.type==pygame.QUIT:
@@ -17,27 +87,22 @@ while True:
 			sys.exit()
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_w:
-				y-=150
+				pl.jump = True
 			elif event.key == pygame.K_d:
-				x+=30
+				pl.state = "right"
 			elif event.key == pygame.K_a:
-				x-=30
+				pl.state = "left"
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_a or event.key == pygame.K_d:
+				pl.state = "stop"
+			elif event.key == pygame.K_w:
+				pl.jump = False 
+
 
 	s.fill(BLUE)
-	if not (y+25 >= 490):
-		y+=3
-	if not (y-25 >= 400):
-		y+=0
-	elif not (x+25 <= 500):
-		x+=0
-
-		
-	
-
-	pygame.draw.rect(s, PLOTFORM,  (500,400,100,25))
-	pygame.draw.rect(s, BEX, (x,y,25,25))
-	pygame.draw.rect(s, BEX, (x,y,25,25))
-	pygame.draw.rect(s, BBLUE, (0,490,750,10))
+	gr.draw()
+	gr1.draw()
+	pl.draw()
 	pygame.display.update()
 	clock.tick(FPS)
 		
